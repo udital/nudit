d871b38db0b532dbfa0c203befbcedd50142674f
myoun <aiden080605@gmail.com>
2022-05-09 19:46:15 +0900

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get("/to")
+  getTo(): string {
+    return "Hello"
+  }
+
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type : 'mariadb',
+      host : 'ssh.cube.dev',
+      port : 3306,
+      username : 'cube',
+      password : 'pqvC_5Nd7=mc@^U+MM-*',
+      
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
